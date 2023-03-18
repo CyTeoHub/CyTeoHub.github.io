0247c2067c5939c98ee2c01be4d48a3fa5755bf7
Kenshin_Bryz <84550697+BryLim-AI@users.noreply.github.com>
2023-03-18 20:41:30 +0800

    Initial Files
    Initial Files

---
+++ docs/index.js
@@ -0,0 +1,24 @@
+
+function updateClock() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const timeString = `${hours}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    const currentTimeElement = document.getElementById('current-time');
+    currentTimeElement.innerHTML = timeString;
+  }
+
+  // Call the updateClock function every second
+  setInterval(updateClock, 1000);
+
+  // const now = new Date(); // Create a new Date object with the current date and time
+  // const currentDate = now.toLocaleDateString();
+  // 
+  // 
+
+  const dateNow = moment().format("MMM Do YY");   
+  const day = moment().format('dddd');    
+  const currentTimeElement_Date = document.getElementById('current-date');
+  currentTimeElement_Date.innerHTML = dateNow +' | '+ day
